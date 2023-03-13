f0b39d2ffea1f7978ed3e963afe0cf11bbd787f0
kim eun gyeong <dmsrud518@naver.com>
2023-03-13 18:04:36 +0900

    coding test practice

---
+++ programmers/[3차]파일명정렬.py
@@ -0,0 +1,6 @@
+import re
+def solution(files):
+    files = [re.split(r"([0-9]+)", file) for file in files]
+    files = sorted(files, key = lambda x : (x[0].lower(), int(x[1]) ) )
+    
+    return ["".join(i) for i in files]
